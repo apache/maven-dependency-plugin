986f7af7f0b0d0f31f5950551a478638dca7ffd1
Alex Collins <alex.e.c@gmail.com>
2018-07-14 16:35:41 +0100

    MDEP-621 Adds un-used artifacts

--- src/main/java/org/apache/maven/plugins/dependency/analyze/FixWarningsMojo.java
+++ src/main/java/org/apache/maven/plugins/dependency/analyze/FixWarningsMojo.java
@@ -18,10 +18,10 @@ package org.apache.maven.plugins.dependency.analyze;
  * under the License.
  */
 
+import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
@@ -34,9 +34,22 @@ import org.codehaus.plexus.component.repository.exception.ComponentLookupExcepti
 import org.codehaus.plexus.context.Context;
 import org.codehaus.plexus.context.ContextException;
 import org.codehaus.plexus.personality.plexus.lifecycle.phase.Contextualizable;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.*;
-import java.util.function.Consumer;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * Attempts to fix the warnings identified by analysis.
@@ -58,13 +71,17 @@ public class FixWarningsMojo extends AbstractMojo implements Contextualizable {
     @Parameter(property = "analyzer", defaultValue = "default")
     private String analyzer;
 
+    @Parameter(property = "indent", defaultValue = "    ")
+    private String indent;
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
 
         if (project.getPackaging().equals("pom")) {
-            getLog().info("skipping -pom packaging");
+            getLog().info("skipping - pom packaging");
             return;
         }
+
         try {
             executeAux();
         } catch (Exception e) {
@@ -74,20 +91,88 @@ public class FixWarningsMojo extends AbstractMojo implements Contextualizable {
 
     private void executeAux() throws Exception {
 
-
+        getLog().info("Step 1 - verify project");
         verify();
         ProjectDependencyAnalysis analysis = getAnalyzer().analyze(project);
 
+        getLog().info("Step 2 - add used un-declared artifacts");
+        addUnusedDependencies(analysis.getUsedUndeclaredArtifacts());
+    }
+
+    private void addUnusedDependencies(Set<Artifact> usedUndeclaredArtifacts) throws SAXException, IOException, ParserConfigurationException, TransformerException {
+        File pom = new File(baseDir, "pom.xml");
+        Document doc = DocumentBuilderFactory.newInstance()
+                .newDocumentBuilder()
+                .parse(pom);
+        Node dependencies = getDependencies(doc);
+        for (Artifact artifact : new TreeSet<>(usedUndeclaredArtifacts)) {
+            getLog().info("+ " + artifact);
+            append2Indents(doc, dependencies);
+            Element dependency = doc.createElement("dependency");
+            dependencies.appendChild(dependency);
+            appendNewLine(doc, dependency);
+
+            append4Indents(doc, dependency);
+            dependency.appendChild(doc.createComment("automatically added"));
+            appendNewLine(doc, dependency);
+
+            append4Indents(doc, dependency);
+            Element groupId = doc.createElement("groupId");
+            groupId.appendChild(doc.createTextNode(artifact.getGroupId()));
+            dependency.appendChild(groupId);
+            appendNewLine(doc, dependency);
+
+            append4Indents(doc, dependency);
+            Element artifactId = doc.createElement("artifactId");
+            artifactId.appendChild(doc.createTextNode(artifact.getGroupId()));
+            dependency.appendChild(artifactId);
+            appendNewLine(doc, dependency);
+
+            append4Indents(doc, dependency);
+            Element version = doc.createElement("version");
+            version.appendChild(doc.createTextNode(artifact.getVersion()));
+            dependency.appendChild(version);
+            appendNewLine(doc, dependency);
+            append2Indents(doc, dependency);
+
+            dependencies.appendChild(doc.createTextNode("\n"));
+        }
+
+
+        TransformerFactory.newInstance().newTransformer()
+                .transform(new DOMSource(doc), new StreamResult(pom));
+    }
+
+    private Node append2Indents(Document doc, Node dependencies) {
+        return dependencies.appendChild(doc.createTextNode(indent + indent));
+    }
+
+    private Node append4Indents(Document doc, Element dependency) {
+        return dependency.appendChild(doc.createTextNode(indent + indent + indent));
+    }
+
+    private Node appendNewLine(Document doc, Element dependency) {
+        return dependency.appendChild(doc.createTextNode("\n"));
+    }
+
+    private Node getDependencies(Document doc) {
+        NodeList childNodes = doc.getDocumentElement().getChildNodes();
+        for (int i = 0; i < childNodes.getLength(); i++) {
+            Node item = childNodes.item(i);
+            if (item.getNodeName().equals("dependencies")) {
+                return item;
+            }
+        }
+        return null;
     }
 
     private void verify() throws IOException, InterruptedException {
-
 
         getLog().info("verifying " + baseDir);
 
         Process process = new ProcessBuilder()
                 .directory(baseDir)
-                .command("mvn", "clean", "install")
+                .command("mvn", "-q", "clean", "test-compile")
                 .start();
 
         log(process.getInputStream(), new LogConsumer() {
