e86cbe2f4ce9d7ae5b1236e235cb4cd6997c4cce
jonvolfson <37542432+jonvolfson@users.noreply.github.com>
2020-06-17 14:05:08 +0000

    [MDEP-645] Created new mojo GetClassesMojo which lists all class dependencies for a specified artifact. (#57)
    * Created new mojo GetClassesMojo which lists all class dependencies for a specified artifact
    
    * adding test file and fixing small function name
    
    * Updating test functions names to be self-describing
    
    * Fixing styling issues preventing build completion
    
    * Removed return statements from test cases, removed unneeded piece of recycled code for building the ProjectBuildRequest
    
    * Changed GetClassesMojo to ListClassesMojo, fixed comments, changed transitive default value to false, changed ListClassesMojo methods to private if able, replaced assert in test case.
    
    * Changed modifying methods to private since outside classes do not interact with the mojo, fixed comments to follow oracle javadoc guidelines, changes method names to be more descriptive of their purpose, fixed shadowing variable names and renamed variables to be more descriptive
    
    * Removed unneccessary setter and getter methods, made the mojo in the test class a private field, included printing logic to use a try-with-resource statement to guarantee JarFile closure, reverted thrown error message when resolving dependencies to original to be less confusing.

---
+++ src/test/java/org/apache/maven/plugins/dependency/TestGetClassesMojo.java
@@ -0,0 +1,87 @@
+package org.apache.maven.plugins.dependency;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.LegacySupport;
+import org.apache.maven.plugin.testing.stubs.MavenProjectStub;
+import org.apache.maven.settings.Server;
+import org.apache.maven.settings.Settings;
+import org.junit.Assert;
+import org.sonatype.aether.impl.internal.SimpleLocalRepositoryManager;
+import org.sonatype.aether.util.DefaultRepositorySystemSession;
+
+import java.io.File;
+
+public class TestGetClassesMojo
+        extends AbstractDependencyMojoTestCase
+{
+    private ListClassesMojo mojo;
+
+    protected void setUp()
+            throws Exception
+    {
+        // required for mojo lookups to work
+        super.setUp( "markers", false );
+
+        File testPom = new File( getBasedir(), "target/test-classes/unit/get-test/plugin-config.xml" );
+        assertTrue(testPom.exists());
+        mojo = (ListClassesMojo) lookupMojo( "list-classes", testPom );
+
+        assertNotNull( mojo );
+
+        LegacySupport legacySupport = lookup( LegacySupport.class );
+        MavenSession session = newMavenSession( new MavenProjectStub() );
+        Settings settings = session.getSettings();
+        Server server = new Server();
+        server.setId( "myserver" );
+        server.setUsername( "foo" );
+        server.setPassword( "bar" );
+        settings.addServer( server );
+        legacySupport.setSession( session );
+        DefaultRepositorySystemSession repoSession =
+                (DefaultRepositorySystemSession) legacySupport.getRepositorySession();
+        repoSession.setLocalRepositoryManager( new SimpleLocalRepositoryManager( testDir.getAbsolutePath() ) );
+
+        setVariableValueToObject( mojo, "session", legacySupport.getSession() );
+    }
+
+    public void testGetClassesNotTransitive()
+            throws Exception
+    {
+        setVariableValueToObject( mojo, "remoteRepositories", "central::default::https://repo.maven.apache.org/maven2,"
+                + "central::::https://repo.maven.apache.org/maven2," + "https://repo.maven.apache.org/maven2" );
+        setVariableValueToObject( mojo, "artifact", "org.apache.commons:commons-lang3:3.6" );
+        setVariableValueToObject( mojo, "transitive", Boolean.FALSE );
+
+        mojo.execute();
+    }
+
+    public void testGetClassesTransitive()
+            throws Exception
+    {
+        setVariableValueToObject( mojo, "remoteRepositories", "central::default::https://repo.maven.apache.org/maven2,"
+                + "central::::https://repo.maven.apache.org/maven2," + "https://repo.maven.apache.org/maven2" );
+        setVariableValueToObject( mojo, "artifact", "org.apache.commons:commons-lang3:3.6" );
+        setVariableValueToObject( mojo, "transitive", Boolean.TRUE );
+
+        mojo.execute();
+    }
+}
