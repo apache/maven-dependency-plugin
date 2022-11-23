f2e192ae263531e50948082d4fb544cac43f9fa9
Slawomir Jaranowski <s.jaranowski@gmail.com>
2022-11-24 00:02:15 +0100

    [MDEP-809] Fix JavaDoc for verbose parameter of tree mojo

--- src/main/java/org/apache/maven/plugins/dependency/tree/TreeMojo.java
+++ src/main/java/org/apache/maven/plugins/dependency/tree/TreeMojo.java
@@ -157,9 +157,7 @@ public class TreeMojo
     private String scope;
 
     /**
-     * Whether to include omitted nodes in the serialized dependency tree. Notice this feature actually uses Maven 2
-     * algorithm and <a href="https://maven.apache.org/shared/maven-dependency-tree/">may give wrong results when used
-     * with Maven 3</a>.
+     * Whether to include omitted nodes in the serialized dependency tree.
      *
      * @since 2.0-alpha-6
      */
