04c32164bcb3ecef6115616fee291d5ee071d370
Michael Osipov <michaelo@apache.org>
2024-06-07 23:54:02 +0200

    [MDEP-939] Lock down classifier in dependency:sources goal
    This closes #409

--- src/main/java/org/apache/maven/plugins/dependency/resolvers/ResolveDependencySourcesMojo.java
+++ src/main/java/org/apache/maven/plugins/dependency/resolvers/ResolveDependencySourcesMojo.java
@@ -18,9 +18,9 @@
  */
 package org.apache.maven.plugins.dependency.resolvers;
 
-import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 
 /**
@@ -36,19 +36,10 @@ import org.apache.maven.plugins.annotations.ResolutionScope;
         threadSafe = true)
 public class ResolveDependencySourcesMojo extends ResolveDependenciesMojo {
 
-    private static final String SOURCE_CLASSIFIER = "sources";
+    private static final String SOURCES_CLASSIFIER = "sources";
 
-    /**
-     * Main entry into mojo. Gets the list of dependencies and iterates through resolving the source jars.
-     *
-     * @throws MojoExecutionException with a message if an error occurs.
-     */
-    @Override
-    protected void doExecute() throws MojoExecutionException {
-        if (this.classifier == null || this.classifier.isEmpty()) {
-            this.classifier = SOURCE_CLASSIFIER;
-        }
-
-        super.doExecute();
+    @Parameter(name = "classifier", defaultValue = SOURCES_CLASSIFIER, readonly = true)
+    public void setClassifier(String classifier) {
+        this.classifier = classifier;
     }
 }
