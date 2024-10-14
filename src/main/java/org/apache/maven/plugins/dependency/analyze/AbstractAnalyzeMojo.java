517a6c8b90cbe34af032f9b0778a31f48756768b
Elliotte Rusty Harold <elharo@ibiblio.org>
2024-10-14 08:01:45 -0400

    Clean up parameters docs

--- src/main/java/org/apache/maven/plugins/dependency/analyze/AbstractAnalyzeMojo.java
+++ src/main/java/org/apache/maven/plugins/dependency/analyze/AbstractAnalyzeMojo.java
@@ -74,22 +74,20 @@ public abstract class AbstractAnalyzeMojo extends AbstractMojo {
      * <a href="/shared/maven-dependency-analyzer/">maven-dependency-analyzer</a> is used. To use this, you must declare
      * a dependency for this plugin that contains the code for the analyzer. The analyzer must have a declared Plexus
      * role name, and you specify the role name here.
-     *
-     * @since 2.2
      */
-    @Parameter(property = "analyzer", defaultValue = "default")
+    @Parameter(defaultValue = "default")
     private String analyzer;
 
     /**
      * Whether to fail the build if a dependency warning is found.
      */
-    @Parameter(property = "failOnWarning", defaultValue = "false")
+    @Parameter(defaultValue = "false")
     private boolean failOnWarning;
 
     /**
      * Output used dependencies.
      */
-    @Parameter(property = "verbose", defaultValue = "false")
+    @Parameter(defaultValue = "false")
     private boolean verbose;
 
     /**
@@ -97,7 +95,7 @@ public abstract class AbstractAnalyzeMojo extends AbstractMojo {
      * <p>
      * <code><b>Non-test scoped</b></code> list will be not affected.
      */
-    @Parameter(property = "ignoreNonCompile", defaultValue = "false")
+    @Parameter(defaultValue = "false")
     private boolean ignoreNonCompile;
 
     /**
@@ -105,7 +103,7 @@ public abstract class AbstractAnalyzeMojo extends AbstractMojo {
      *
      * @since 3.2.0
      */
-    @Parameter(property = "ignoreUnusedRuntime", defaultValue = "false")
+    @Parameter(defaultValue = "false")
     private boolean ignoreUnusedRuntime;
 
     /**
@@ -114,47 +112,37 @@ public abstract class AbstractAnalyzeMojo extends AbstractMojo {
      * the <i>Non-test scoped test only dependencies found</i> warning to the
      * <code>&lt;ignoredNonTestScopedDependencies&gt;</code> configuration.
      *
-     * @since 3.3.1-SNAPSHOT
+     * @since 3.3.1
      */
-    @Parameter(property = "ignoreAllNonTestScoped", defaultValue = "false")
+    @Parameter(defaultValue = "false")
     private boolean ignoreAllNonTestScoped;
 
     /**
-     * Output the xml for the missing dependencies (used but not declared).
-     *
-     * @since 2.0-alpha-5
+     * Output XML for the missing dependencies (used but not declared).
      */
-    @Parameter(property = "outputXML", defaultValue = "false")
+    @Parameter(defaultValue = "false")
     private boolean outputXML;
 
     /**
      * Output scriptable values for the missing dependencies (used but not declared).
-     *
-     * @since 2.0-alpha-5
      */
-    @Parameter(property = "scriptableOutput", defaultValue = "false")
+    @Parameter(defaultValue = "false")
     private boolean scriptableOutput;
 
     /**
      * Flag to use for scriptable output.
-     *
-     * @since 2.0-alpha-5
      */
-    @Parameter(property = "scriptableFlag", defaultValue = "$$$%%%")
+    @Parameter(defaultValue = "$$$%%%")
     private String scriptableFlag;
 
     /**
-     * Flag to use for scriptable output
-     *
-     * @since 2.0-alpha-5
+     * Base directory for output.
      */
     @Parameter(defaultValue = "${basedir}", readonly = true)
     private File baseDir;
 
     /**
-     * Target folder
-     *
-     * @since 2.0-alpha-5
+     * Target folder.
      */
     @Parameter(defaultValue = "${project.build.directory}", readonly = true)
     private File outputDirectory;
@@ -162,16 +150,12 @@ public abstract class AbstractAnalyzeMojo extends AbstractMojo {
     /**
      * Force dependencies as used, to override incomplete result caused by bytecode-level analysis. Dependency format is
      * <code>groupId:artifactId</code>.
-     *
-     * @since 2.6
      */
     @Parameter
     private String[] usedDependencies;
 
     /**
      * Skip plugin execution completely.
-     *
-     * @since 2.7
      */
     @Parameter(property = "mdep.analyze.skip", defaultValue = "false")
     private boolean skip;
@@ -190,8 +174,6 @@ public abstract class AbstractAnalyzeMojo extends AbstractMojo {
      * For example, <code>org.apache.*</code> will match all artifacts whose group id starts with
      * <code>org.apache.</code>, and <code>:::*-SNAPSHOT</code> will match all snapshot artifacts.
      * </p>
-     *
-     * @since 2.10
      */
     @Parameter
     private String[] ignoredDependencies = new String[0];
@@ -209,8 +191,6 @@ public abstract class AbstractAnalyzeMojo extends AbstractMojo {
      * For example, <code>org.apache.*</code> will match all artifacts whose group id starts with
      * <code>org.apache.</code>, and <code>:::*-SNAPSHOT</code> will match all snapshot artifacts.
      * </p>
-     *
-     * @since 2.10
      */
     @Parameter
     private String[] ignoredUsedUndeclaredDependencies = new String[0];
@@ -228,8 +208,6 @@ public abstract class AbstractAnalyzeMojo extends AbstractMojo {
      * For example, <code>org.apache.*</code> matches all artifacts whose group id starts with
      * <code>org.apache.</code>, and <code>:::*-SNAPSHOT</code> will match all snapshot artifacts.
      * </p>
-     *
-     * @since 2.10
      */
     @Parameter(defaultValue = "org.slf4j:slf4j-simple::")
     private String[] ignoredUnusedDeclaredDependencies;
