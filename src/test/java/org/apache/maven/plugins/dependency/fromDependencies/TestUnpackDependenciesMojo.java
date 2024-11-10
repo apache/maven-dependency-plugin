e0fb53fbe5999b2929aa705d6ebb4bd0f92917f7
Elliotte Rusty Harold <elharo@ibiblio.org>
2024-11-10 11:51:43 -0500

    spotless

--- src/test/java/org/apache/maven/plugins/dependency/fromDependencies/TestUnpackDependenciesMojo.java
+++ src/test/java/org/apache/maven/plugins/dependency/fromDependencies/TestUnpackDependenciesMojo.java
@@ -20,7 +20,6 @@ package org.apache.maven.plugins.dependency.fromDependencies;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
