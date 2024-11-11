5cd4072e45791ea90760a1a5f697013e3601e7ad
Elliotte Rusty Harold <elharo@ibiblio.org>
2024-11-11 06:34:04 -0500

    Expand test

--- src/it/projects/analyze/verify.groovy
+++ src/it/projects/analyze/verify.groovy
@@ -36,6 +36,7 @@ assert buildLog.contains( '[WARNING]    org.apache.maven:maven-repository-metada
 assert buildLog.contains( '[WARNING]       class org.apache.maven.artifact.repository.metadata.Metadata')
 assert buildLog.contains( '[WARNING] Unused declared dependencies found:')
 assert buildLog.contains( '[WARNING]    org.apache.maven:maven-project:jar:2.0.6:compile')
+assert !buildLog.contains( 'org.slf4j:slf4j-simple')
 
 
 return true
