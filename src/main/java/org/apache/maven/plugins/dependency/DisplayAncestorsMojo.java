8519bab985763fccdf5a715b6da8dec9f8c52266
Elliotte Rusty Harold <elharo@ibiblio.org>
2024-11-17 14:06:05 -0500

    PropertiesMojo

--- src/main/java/org/apache/maven/plugins/dependency/DisplayAncestorsMojo.java
+++ src/main/java/org/apache/maven/plugins/dependency/DisplayAncestorsMojo.java
@@ -18,13 +18,14 @@
  */
 package org.apache.maven.plugins.dependency;
 
+import javax.inject.Inject;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.project.MavenProject;
@@ -42,8 +43,12 @@ public class DisplayAncestorsMojo extends AbstractMojo {
     /**
      * POM
      */
-    @Component
     private MavenProject project;
+
+    @Inject
+    public DisplayAncestorsMojo(MavenProject project) {
+        this.project = project;
+    }
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
