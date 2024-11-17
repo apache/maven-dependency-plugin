0bac7e93dc143efaa42b9db92127ce2c51a050fd
Elliotte Rusty Harold <elharo@ibiblio.org>
2024-11-17 08:45:33 -0500

    AnalyzeDepMgt

--- src/main/java/org/apache/maven/plugins/dependency/analyze/AnalyzeDepMgt.java
+++ src/main/java/org/apache/maven/plugins/dependency/analyze/AnalyzeDepMgt.java
@@ -26,6 +26,8 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.inject.Named;
+
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.model.Dependency;
 import org.apache.maven.model.DependencyManagement;
@@ -33,7 +35,6 @@ import org.apache.maven.model.Exclusion;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
@@ -51,10 +52,7 @@ import org.apache.maven.project.MavenProject;
 public class AnalyzeDepMgt extends AbstractMojo {
     // fields -----------------------------------------------------------------
 
-    /**
-     *
-     */
-    @Component
+    @Named
     private MavenProject project;
 
     /**
