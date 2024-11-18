eb5127486c4cc85b8bc622b7b47db297c52b3275
Elliotte Rusty Harold <elharo@ibiblio.org>
2024-11-17 21:12:14 -0500

    AnalyzeReport

--- src/main/java/org/apache/maven/plugins/dependency/analyze/AnalyzeReport.java
+++ src/main/java/org/apache/maven/plugins/dependency/analyze/AnalyzeReport.java
@@ -18,10 +18,11 @@
  */
 package org.apache.maven.plugins.dependency.analyze;
 
+import javax.inject.Inject;
+
 import java.util.Locale;
 import java.util.Set;
 
-import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Execute;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
@@ -48,7 +49,6 @@ public class AnalyzeReport extends AbstractMavenReport {
     /**
      * The Maven project dependency analyzer to use.
      */
-    @Component
     private ProjectDependencyAnalyzer analyzer;
 
     /**
@@ -87,8 +87,13 @@ public class AnalyzeReport extends AbstractMavenReport {
     /**
      * Internationalization component
      */
-    @Component
     private I18N i18n;
+
+    @Inject
+    public AnalyzeReport(ProjectDependencyAnalyzer analyzer, I18N i18n) {
+        this.analyzer = analyzer;
+        this.i18n = i18n;
+    }
 
     // Mojo methods -----------------------------------------------------------
 
@@ -142,11 +147,13 @@ public class AnalyzeReport extends AbstractMavenReport {
     }
 
     /** {@inheritDoc} */
+    @Override
     public String getName(Locale locale) {
         return getI18nString(locale, "name");
     }
 
     /** {@inheritDoc} */
+    @Override
     public String getDescription(Locale locale) {
         return getI18nString(locale, "description");
     }
